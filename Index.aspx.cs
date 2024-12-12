eaa94578491ec78abacd99f98cc5631d8b6c2de5
Juaanyzy <juanaragon01011@gmail.com>
2024-12-11 23:31:56 -0500

    Update Index.aspx.cs
    Test this, please

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -7,11 +7,23 @@ using System.Web.UI.WebControls;
 
 namespace MelodyHub
 {
-    public partial class index : System.Web.UI.Page
-    {
+    public partial class Index : System.Web.UI.Page
+        {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["usuario"] == null)
+            {
+                Response.Redirect("Login.aspx");
 
+            }
+
+        }
+
+        protected void BtnSalir_Click1(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("Login.aspx", false);
+            Context.ApplicationInstance.CompleteRequest();
         }
     }
 }
